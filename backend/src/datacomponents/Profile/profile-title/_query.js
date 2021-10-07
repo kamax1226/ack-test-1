a776c12385c75f72dbb8f4ec8ad4576d635f3169
Andrei <karlikboroda@gmail.com>
2021-10-07 20:01:12 +0300

    Private Query returning random integer

--- backend/src/datacomponents/Profile/profile-title/_query.js
+++ backend/src/datacomponents/Profile/profile-title/_query.js
@@ -1,11 +1,17 @@
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 export const queryTypes = `
   type Query {
-    test: String
+    _test31: Int
   }
 `;
 
 export const queryResolvers = {
   Query: {
-    //
+    _test31: () => {
+      return randomIntFromInterval(1, 100);
+    }
   },
 };

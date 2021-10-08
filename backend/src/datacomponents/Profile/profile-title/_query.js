af4c1cf0825c71c1226c26bd0e7283b3dc814b8f
Andrei <karlikboroda@gmail.com>
2021-10-08 23:57:43 +0300

    Protected query with scalar EmailAddress

--- backend/src/datacomponents/Profile/profile-title/_query.js
+++ backend/src/datacomponents/Profile/profile-title/_query.js
@@ -1,10 +1,15 @@
-function randomIntFromInterval(min, max) { // min and max included 
+import { roles, permissions } from '~/directives'
+
+const { GraphQLScalarType, Kind } = require('graphql');
+
+const randomIntFromInterval = (min, max) => { // min and max included 
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 export const queryTypes = `
   type Query {
     _test31: Int
+    test32: EmailAddress @${roles.is.user}
   }
 `;
 
@@ -12,6 +17,9 @@ export const queryResolvers = {
   Query: {
     _test31: () => {
       return randomIntFromInterval(1, 100);
+    },
+    test32: () => {
+      return 'info@test.com'
     }
   },
 };
